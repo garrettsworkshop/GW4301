f1cb29671b6c91dde0bf1b8a61d384eb643095b5
Zane Kaminski <zanekaminski@gmail.com>
2020-05-24 23:26:43 -0400

    New take on PLD verilog

--- cpld/CRAM.v
+++ cpld/CRAM.v
@@ -23,9 +23,9 @@ module CRAM	(PHI2, DotClk, nRES,
 	input nWE;
 
 	/* Data Bus */
-	wire DOE = RAMRD;
 	inout [7:0] D;
-	assign D = DOE ? RD[7:0] : 8'bZ;
+	reg [7:0] Dout;
+	assign D = RAMRD ? Dout[7:0] : 8'bZ;
 
 	/* More C64/6502 */
 	input BA;
@@ -37,7 +37,9 @@ module CRAM	(PHI2, DotClk, nRES,
 	/* Select Signals */
 	input nIO1, nIO2, nROML, nROMH;
 	wire RAMSEL = ~nIO1;
+	wire RAMSELpre = A[15:8]==8'hDE;
 	wire RAMRD = RAMSEL & nWE;
+	wire RAMRDpre = RAMSELpre & nWE;
 	wire RAMWE = RAMSEL & ~nWE;
 	wire BlockSEL = ~nIO2 & A[7:0]==8'hFF;
 	wire BlockWE = BlockSEL & ~nWE;
@@ -45,21 +47,18 @@ module CRAM	(PHI2, DotClk, nRES,
 	wire WindowWE = WindowSEL & ~nWE;
 
 	/* DRAM */
-	output nRAS;
-	assign nRAS = ~RASr;
-	output nCAS;
-	assign nCAS = ~((CASr & PHI2) | CASf);
+	output reg nRAS = 1'b1;
+	output reg nCAS = 1'b1;
 	output nRWE;
-	assign nRWE = nWE | ~PHI2;
+	assign nRWE = ~(~nWE & PHI2);
 	output [11:0] RA;
-	assign RA[11:0] = ~RAsel ? 
-		{ 1'b0, Block[6:0], Window[5:2] } : // Row
-		{ 1'b0, Block[7], Window[1:0], A[7:0]}; // Column
-
-	wire RDOE = RAMWE;
+	reg RAsel = 0;
+	assign RA[11] = 1'b0;
+	assign RA[10:0] = ~RAsel ? 
+		{Block[6:0], Window[5:2]} : // Row
+		{Block[7], Window[1:0], A[7:0]}; // Column
 	inout [7:0] RD;
-	assign RD[7:0] = RDOE ? D[7:0] : 8'bZ;
-	reg RASr = 0, CASr = 0, CASf = 0, RAsel = 0;
+	assign RD[7:0] = RAMWE ? D[7:0] : 8'bZ;
 	
 	/* Registers */
 	reg [7:0] Block;
@@ -69,7 +68,7 @@ module CRAM	(PHI2, DotClk, nRES,
 	reg PHI2reg = 0; // Saved PHI2 at last rising clock edge
 	reg nPHI2seen = 0; // Have we seen /PHI2 since POR?
 	reg [3:0] S = 0; // State counter
-	reg [3:0] Ref = 0; // Refresh skip counter
+	reg [2:0] Ref = 0; // Refresh counter
 	
 	always @(posedge DotClk) begin
 		// Synchronize state counter to S1 in first cycle of /PHI2
@@ -79,21 +78,31 @@ module CRAM	(PHI2, DotClk, nRES,
 			S==4'h0 ? 4'h0 :
 			S==4'hF ? 4'hF : S+1;
 		
-		// Refresh counter allows DRAM refresh once every 13 cycles
-		if (S==2) Ref <= (Ref[3:2] == 2'b11) ? 4'h0 : Ref+1;
+		// Refresh counter allows DRAM refresh once every 8 cycles
+		Ref <= Ref+1;
 
 		// DRAM RAS/CAS
-		RASr <= (S==1 & Ref==0) | (S==4 & RAMRD) | (S==5 & RAMSEL) | (S==6 & RAMWE);
-		CASr <= (S==5 & RAMRD) | (S==6 & RAMSEL) | (S==7 & RAMRD);
+		RAS <= (S==2 & Ref==0) | 
+			   (S==4 & RAMRDpre) | (S==5 & RAMSEL) | (S==6 & RAMWE);
+		CAS <= (S==1) & Ref==0) | (S==2 & Ref==0) | 
+			   (S==5 & RAMRD) | (S==6 & RAMWR);
 	end
-	
+
 	always @(negedge DotClk) begin
-		// DRAM CAS and address muxing
+		// DRAM address muxing
 		RAsel <= RAMSEL & (S==5 | S==6);
-		CASf <= S==1 & Ref==0;
 	end
 	
-	always @(posedge DotClk) begin
+	always @(posedge DotClk, negedge nRES) begin
+		if (~nRES) begin
+			Dout <= 0;
+		end else begin
+			// Register output data at end of S6
+			if (S==6 & BlockWE) Dout[7:0] <= RD[7:0];
+		end
+	end
+	
+	always @(posedge DotClk, negedge nRES) begin
 		if (~nRES) begin
 			Block <= 0;
 			Window <= 0;
